97eaa280b6f014c331121396b0fbe97dd546cc24
Jesus Vasquez <jesusvasquez333@gmail.com>
2021-02-01 10:46:15 -0800

    Change reference from master to main, as the branch name was changed

--- scripts/update_release_table.sh
+++ scripts/update_release_table.sh
@@ -7,15 +7,15 @@
 git config --global user.name "GitHub Actions"
 git config --global user.email "github-actions@github.com"
 
-# Checkout the master branch (as we are right now on a tag)
+# Checkout the main branch (as we are right now on a tag)
 git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 git fetch --all
-git checkout master
+git checkout main
 
 # Add the new release row to the release table
 echo "${release_new_row}" >> RELEASES.md
 
-# Commit and push the change to master
+# Commit and push the change to main
 git commit -a -m "Update RELEASES"
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/${REPO_SLUG}
-git push origin master
+git push origin main
